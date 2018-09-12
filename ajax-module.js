3102105f0e9d01ff04bbf29e31012c06683bdedc
Rithika <rithika.chowta@ymedialabs.com>
2018-09-12 15:08:27 +0530

    push

---
+++ ajax-module.js
@@ -0,0 +1,21 @@
+/* Module to make GET request to any API. Returns a method call taking options as parameter
+options{
+  url : URL endpoint to make the request to,
+  callBackFn : reference to callback function to execute on ready state change
+}
+ */
+
+var CALLAPI = (function() {
+    function call(options) {
+      var requestObj = new XMLHttpRequest(),
+        URL = options.url,
+        callBackFn = options.callBackFn;
+      requestObj.onreadystatechange = callBackFn.bind(requestObj);
+      requestObj.open("GET", URL);
+      requestObj.send();
+    }
+  
+    return {
+      call: call
+    };
+  })();
--- script.js
+++ script.js
@@ -1,164 +1,201 @@
-/* Module to make GET request to any API. Returns a method call taking options as parameter
-options{
-  url : URL endpoint to make the request to,
-  callBackFn : reference to callback function to execute on ready state change
-}
- */
+// custom shorthand function to get elements
+var $ = document.querySelector.bind(document);
 
-var CALLAPI = (function() {
-  function call(options) {
-    var requestObj = new XMLHttpRequest(),
-      URL = options.url,
-      callBackFn = options.callBackFn;
-    requestObj.onreadystatechange = callBackFn.bind(requestObj);
-    requestObj.open("GET", URL);
-    requestObj.send();
-  }
-
-  return {
-    call: call
-  };
-})();
+// utitlity function to convert string to sentence case
+String.prototype.replaceAt = function(index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  );
+};
 
 /* Module to find weather at any location using openweathermap API. 
 Location can be found by navigator or taking user input
 Returns a function find to find the weather at current location
  */
 
-var WEATHERFINDER = (function() {
-  function find() {
-    var options,
-      data,
-      firstletter,
-      urlParam,
-      input = document.querySelector("#input"),
-      homeContent = document.querySelector("#homeContent");
-    // request user for location access
-    navigator.geolocation.getCurrentPosition(success, error);
-
-    // if successful make API call
-    function success(position, url) {
-      urlParam =
-        url ||
-        "https://api.openweathermap.org/data/2.5/weather?lat=" +
-          position.coords.latitude +
-          "&lon=" +
-          position.coords.longitude +
-          "&appid=ccb7a356a2bf2fa6d99bf8c4ac2b373a&units=metric";
-      options = {
-        url: urlParam,
-        callBackFn: callback
-      };
-      CALLAPI.call(options);
+var WeatherFinder = (function() {
+  var private_elements = {
+    ids: {
+      inputContainer: "#inputContainer",
+      homeContainer: "#homeContainer",
+      locationInput: "#inputContainer input",
+      inputBtn: "#inputContainer button",
+      status: "#homeContainer p",
+      city: "#location",
+      description: "#description",
+      weather: "#weather",
+      results: "#results",
+      rainyAnimation: "#rainy",
+      cloudyAnimation: "#cloudy",
+      clearanimation: "#clear",
+      snowyAnimation: "#snow"
     }
+  };
 
-    // in case user denies access, ask for explicit input
-    function error(err) {
-      var locationInput = document.querySelector("#input input"),
-        query;
-      render([
-        { element: input, style: "display:block;" },
-        { element: homeContent, style: "display:none;" }
+  // function constructor
+  var checkWeather = function() {};
+
+  // request user for location access
+  checkWeather.prototype.init = function() {
+    navigator.geolocation.getCurrentPosition(
+      this.success.bind(this),
+      this.error.bind(this)
+    );
+  };
+
+  // if successful make API call
+  checkWeather.prototype.success = function(position) {
+  
+    var options = {
+      url: this.getUrl(position),
+      callBackFn: this.callback
+    };
+    CALLAPI.call(options);
+  };
+
+  // in case user denies access, ask for explicit input
+  checkWeather.prototype.error = function(err) {
+    var locationInput = $(private_elements.ids.locationInput);
+    this.render([
+      {
+        element: $(private_elements.ids.inputContainer),
+        style: "display:block;"
+      },
+      { element: $(private_elements.ids.homeContainer), style: "display:none;" }
+    ]);
+    $(private_elements.ids.inputBtn).addEventListener("click", callWeather);
+
+    // adding feature to retrieve results on enter key press
+    locationInput.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") callWeather.call(this);
+    });
+
+    // clearing placeholder on click
+    locationInput.addEventListener("click", function() {
+      locationInput.placeholder = "";
+    });
+
+    // calls success function with url containing explicit location
+    function callWeather() {
+      this.render([
+        {
+          element: $(private_elements.ids.inputContainer),
+          style: "display:none"
+        }
       ]);
-      document
-        .querySelector("#input button")
-        .addEventListener("click", callWeather);
+      this.success(locationInput.value);
+    }
+  };
 
-      //adding feature to retrieve results on enter key press
-      locationInput.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") callWeather();
-      });
+  checkWeather.prototype.getUrl = function(value) {
+    if (value.coords) {
+      return (
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        value.coords.latitude +
+        "&lon=" +
+        value.coords.longitude +
+        "&appid=ccb7a356a2bf2fa6d99bf8c4ac2b373a&units=metric"
+      );
+    } else {
+      return (
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        value +
+        "&appid=ccb7a356a2bf2fa6d99bf8c4ac2b373a&units=metric"
+      );
+    }
+  };
 
-      // calls success function with url containing explicit location
-      function callWeather() {
-        $("#input").hide();
-        query =
-          "https://api.openweathermap.org/data/2.5/weather?q=" +
-          locationInput.value +
-          "&appid=ccb7a356a2bf2fa6d99bf8c4ac2b373a&units=metric";
-        success(null, query);
-      }
+  // render response data with animations
+  checkWeather.prototype.callback = function() {
+    // updating status
+    if (this.readyState == XMLHttpRequest.OPENED) {
+      $(private_elements.ids.status).innerHTML = "Getting weather data...";
     }
 
-    // render response data with animations
-    function callback() {
-      // updating status
-      if (this.readyState == XMLHttpRequest.OPENED) {
-        document.querySelector("#homeContent p").innerHTML =
-          "Getting weather data...";
-      }
-
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        data = JSON.parse(this.responseText);
-        firstletter = data.weather[0].description.charAt(0).toUpperCase();
-        render([
-          { element: homeContent, style: "display:none" },
-          {
-            element: document.querySelector("#results"),
-            style: "display:block"
-          }
-        ]);
-        var weatherCategory = data.weather[0].main;
-        animator(weatherCategory);
-        document.querySelector("#location").innerHTML = data.name;
-        document.querySelector(
-          "#weather"
-        ).innerHTML = data.weather[0].description.replaceAt(0, firstletter);
-        document.querySelector("#description").innerHTML =
-          " <br> Temperature: " +
-          data.main.temp +
-          " °C<br> Humidity: " +
-          data.main.humidity +
-          "%<br> Wind speed: " +
-          data.wind.speed +
-          " m/sec";
-      }
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+       var data = JSON.parse(this.responseText),
+        firstletter = data.weather[0].description.charAt(0).toUpperCase(),
+        weatherCategory = data.weather[0].main;
+        console.log(data);
+      this.render([
+        {
+          element: $(private_elements.ids.homeContainer),
+          style: "display:none"
+        },
+        {
+          element: $(private_elements.ids.results),
+          style: "display:block"
+        }
+      ]);
+      this.animate.call(this, weatherCategory);
+      $(private_elements.ids.city).innerHTML = data.name;
+      $(weather).innerHTML = data.weather[0].description.replaceAt(
+        0,
+        firstletter
+      );
+      $(private_elements.ids.description).innerHTML =
+        " <br> Temperature: " +
+        data.main.temp +
+        " °C<br> Humidity: " +
+        data.main.humidity +
+        "%<br> Wind speed: " +
+        data.wind.speed +
+        " m/sec";
     }
-  }
+    }
 
   // selects animation according to weather category
-  function animator(weatherCategory) {
+  checkWeather.prototype.animate = function(weatherCategory) {
     switch (weatherCategory) {
       case "Thunderstorm":
       case "Drizzle":
       case "Rain":
-        $("#rainy").show();
+        this.render([
+          {
+            element: $(private_elements.ids.rainyAnimation),
+            style: "display:block"
+          }
+        ]);
         break;
       case "Clear":
-        $("#clear").show();
+        this.render([
+          {
+            element: $(private_elements.ids.clearAnimation),
+            style: "display:block"
+          }
+        ]);
         break;
       case "Clouds":
       case "Mist":
       case "Haze":
-        $("#cloudy").show();
+        this.render([
+          {
+            element: $(private_elements.ids.cloudyAnimation),
+            style: "display:block"
+          }
+        ]);
         break;
       case "Snow":
-        $("#snow").show();
+        this.render([
+          {
+            element: $(private_elements.ids.snowyAnimation),
+            style: "display:block"
+          }
+        ]);
         break;
     }
-  }
-  // utitlity function to convert string to sentence case
-  String.prototype.replaceAt = function(index, replacement) {
-    return (
-      this.substr(0, index) +
-      replacement +
-      this.substr(index + replacement.length)
-    );
   };
-
   // utility function to render elements
-  function render(elements) {
+  checkWeather.prototype.render = function(elements) {
+    console.log(elements);
     for (i = 0; i < elements.length; i++) {
-      elements[i].element.style= elements[i].style;
+      elements[i].element.style = elements[i].style;
     }
-  }
-
-  return {
-    find: find
   };
+
+  return new checkWeather();
 })(CALLAPI);
 
-// main application module
-var MAINAPP = (function() {
-  WEATHERFINDER.find();
-})(WEATHERFINDER);
+WeatherFinder.init.call(WeatherFinder);
