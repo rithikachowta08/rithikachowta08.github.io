f9b17787cadd077c7f28a1ace7ed787b7bdd6495
rithikachowta08 <rithikachowta08@gmail.com>
2024-07-16 12:54:31 +0530

    Optimise bg loading

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import Home from "@/pages/Home";
 export default function Main() {
   return (
     <main className="min-h-screen flex flex-col">
+      <img
+        style={{ display: "none" }}
+        fetchPriority="high"
+        src="/images/bg.webp"
+        alt="Bg image"
+      ></img>
       <Home />
       <About />
     </main>
