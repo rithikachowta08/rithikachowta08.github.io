24627feb349e380239d36205cf5699723351bb0a
rithikachowta08 <rithikachowta08@gmail.com>
2025-07-24 21:49:47 +0530

    Transition image

--- components/Home/ProfileImage.tsx
+++ components/Home/ProfileImage.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import "./Home.scss";
+import { useState } from "react";
 
 function ProfileImage() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    console.log("here");
+    setImageLoaded(true);
+  };
+
   return (
     <div className="relative w-full 2xl:w-[60%] aspect-[1.2/1] lg:aspect-square">
       <picture>
         <source srcSet="/images/profile_1x.jpg" media="(max-width: 1024px)" />
         <Image
-          className="profile z-[1] lg:clip-path-trapezium"
+          className={`profile z-[1] lg:clip-path-trapezium${imageLoaded ? " visible" : ""}`}
+          onLoadingComplete={handleImageLoad}
           priority
           src="/images/profile_2x.jpg"
           layout="fill"
